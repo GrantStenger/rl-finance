01110c4c6011121c6fc241c5c05e65621d73c400
Jincheng Zhou <jinchenz@usc.edu>
2019-03-03 14:05:22 -0800

    fix bugs

--- tradinggym/trading_gym/series_envs/series_env.py
+++ tradinggym/trading_gym/series_envs/series_env.py
@@ -3,12 +3,12 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class State:
-    def __init__(self, ):
-        df = self.read_csv("../data/AAPL-Updated.csv")
+    def __init__(self, df):
+        # df = self.read_csv("../data/AAPL-Updated.csv")
         self.df = df
         self.index = 0
 
-    def reset():
+    def reset(self):
         self.index = 0
 
     def next(self): # updates the current index to look at when feeding observations in step function
@@ -18,24 +18,25 @@ class State:
         self.index += 1
         return values, False
 
-    def __get_curr_price():
-        return self.df.ix[self.index,'Open']
+    def __get_curr_price(self):
+        return self.df.ix[self.index, 'Open']
 
-    def shape():
+    def shape(self):
         return self.df.shape
 
-class (gym.Env):
+class SeriesEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, data, episode_len, initial_cash):
-        # self.states = []
-        self.state = None
-        self.bound = 1000000
-        self.initial_cash = initial_cash    # The number of cash the agent has at the start of each episode
-        self.current_cash = None            # The number of cash the agent currently possesses at this timestep
-        self.num_shares = None              # The number of shares the agent currently possesses at this timestep
-        # self.current_price = None           # The current stock price at this timestep
-        # self.current_volume = None          # The volume of the stock at this timestep
+    def __init__(self):
+        pass
+
+    def init(self, data, episode_len, initial_cash):
+        self.state = State(data)
+
+        self.initial_cash = initial_cash        # The number of cash the agent has at the start of each episode
+        self.current_cash = initial_cash        # The number of cash the agent currently possesses at this timestep
+        self.num_shares = 0                     # The number of shares the agent currently possesses at this timestep
+
         self.observation_space = spaces.Box(low=0, high=self.initial_cash, shape=(1,))
         self.decision_space = spaces.Discrete(3)
         self.action_space = spaces.Box(low=0, high=1)
@@ -47,32 +48,46 @@ class (gym.Env):
         self.act_change_price = None        # The current change in price by last action
         '''
 
-    def step(self, action, current_shares):
-        # Action: Hold, Buy, Sell
+    def step(self, action):
+        # Action: action is a 2-element tuple.
+        #   action[0]: {0: Hold, 1: Buy, 2: Sell}
+        #   the meaning of action[1] depends on the value of action[0]:
+        #       action[0] == 0 (Hold): action[1] indicates nothing
+        #       action[0] == 1 (Buy): action[1] is a float in (0, 1) indicating the percentage of realized cash
+        #           (cash in bank account) the agent decides to trade
+        #       action[0] == 2 (Sell): action[1] is an positive integer indicating the number of shares the agent
+        #           decides to sell
+        #
         # current_shares: the current number of shares we are dealing with (buy or sell)
+        #
         # Observation : [current_cash, num_shares, %change(last min), %change(last action), current_price, volume]
+        #
         # Return value: obs, reward, done, info
         #   - obs: Observation
         #   - reward: reward at this timestep
         #   - done: True or False, indicating whether this episode has reached the end
         #   - info: don't know, don't care
 
-        price = self.state.__get_curr_price() # current price
-        cost = current_shares * price # how much cost we are dealing with
+        assert len(action) == 2, "the action argument must be a 2-element tuple"
+        decision = action[0]
+        action_value = action[1]
+
+        # get current stock price
+        price = self.state.__get_curr_price()
+
+        # calculate previous portfolio
         previous_portfolio = self.current_cash + self.num_shares * price
 
-        if action == 0: #Hold
-            pass
-
-        elif action == 1: #Buy
-            if cost<=current_cash:
-                self.num_shares += current_shares
-                self.current_cash -= cost
-
-
-        elif action == 2: #Sell
-            self.num_shares -= current_shares
-            self.current_cash += cost
+        # Buy
+        if decision == 1:
+            current_share = action_value * self.current_cash // price
+            self.num_shares += current_share
+            self.current_cash -= current_share * price
+        # Sell
+        elif decision == 2:
+            current_share = action_value
+            self.num_shares -= current_share
+            self.current_cash += current_share * price
 
         state, done = self.state.next()
         new_price = price
@@ -82,16 +97,16 @@ class (gym.Env):
 
         new_equity = new_price * self.num_shares    # value of stock
         current_portfolio = new_equity + self.current_cash
+
+        # TODO: Reward schedule
         reward = ((current_portfolio - previous_portfolio)/previous_portfolio)*100.0
 
         return state, reward, done, None
 
     def reset(self):
-        self.state = State()
-        self.initial_cash = 1000000
+        self.state.reset()
         state, done = self.state.next()
         return state
-
 
     def render(self, mode='human'):
         pass
